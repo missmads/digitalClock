8a8db20c4fe6b99d117c3dd95defd079aaed567d
Mads Kildegaard Kristjansen <m.i.s.s.mads@hotmail.com>
2019-03-06 13:29:43 +0100

    now?

--- DigitalClock/Properties/AssemblyInfo.cs
+++ DigitalClock/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f5ec86fe-ac92-450f-aa3c-d6358aee7bf2")]
+[assembly: Guid("4b56f8fd-ec04-4b76-b8a8-c5187de84f13")]
 
 // Version information for an assembly consists of the following four values:
 //
